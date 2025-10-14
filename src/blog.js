85603cec59d53f4dcbc0301e8c2f8ca060d856ed
Travis Biehle <travissbiehle@gmail.com>
2025-10-14 09:30:49 -0700

    feat: updated src code to include images in blog thumbnails and reformatted thumbnails

--- src/blog.js
+++ src/blog.js
@@ -1,19 +1,19 @@
 var blogs = [
     {
-        title: "funny cat",
-        date: "10-09-2025",
-        description: "click for a surpise...",
-        image: "",
-        imageAlt: "",
+        title: "Funny cat",
+        date: "10-13-2025",
+        description: "A funny cat.",
+        image: "rigby_cat.webp",
+        imageAlt: "Funny cat from the internet named rigby. He has his tongue out and is making a funny face",
         slug: "funny-cat",
     },
     {
-        title: "blog post 2",
+        title: "Funny dog",
         date: "10-09-2025",
-        description: "now things are really rolling, making lots of blog posts",
-        image: "",
-        imageAlt: "",
-        slug: "blog-post-2", // URL name used to redirect to different page
+        description: "A funny dog.",
+        image: "dog.png",
+        imageAlt: "a dog lying on its back with all its skin folded up and squished.",
+        slug: "funny-dog", // URL name used to redirect to different page
     },
 ];
 var blogContainer = document.getElementById("blog-container");
@@ -35,8 +35,9 @@ blogs.forEach(function (blog) {
     // if there is an image url create an img element
     if (blog.image) {
         var img = document.createElement("img");
-        img.src = blog.image;
+        img.src = "blog/".concat(blog.image);
         img.alt = blog.imageAlt || blog.title;
+        img.style.width = '10em';
         new_post.appendChild(img);
     }
     if (blogContainer)
--- src/blog.ts
+++ src/blog.ts
@@ -9,27 +9,27 @@ type Blog = {
 
 const blogs: Blog[] = [
   {
-    title: "funny cat",
-    date: "10-09-2025",
-    description: "click for a surpise...",
+    title: "Funny cat",
+    date: "10-13-2025",
+    description: "A funny cat.",
     image: "rigby_cat.webp",
     imageAlt: "Funny cat from the internet named rigby. He has his tongue out and is making a funny face",
     slug: "funny-cat",
   },
   {
-    title: "blog post 2",
+    title: "Funny dog",
     date: "10-09-2025",
-    description: "now things are really rolling, making lots of blog posts",
-    image: "",
-    imageAlt: "",
-    slug: "blog-post-2", // URL name used to redirect to different page
+    description: "A funny dog.",
+    image: "dog.png",
+    imageAlt: "a dog lying on its back with all its skin folded up and squished.",
+    slug: "funny-dog", // URL name used to redirect to different page
   },
 ];
 
 const blogContainer = document.getElementById("blog-container");
 blogs.forEach((blog) => {
   const new_post = document.createElement("div");
-  new_post.className = "blog-thumbnail"
+  new_post.className = "blog-thumbnail";
 
   // create blog header element
   const titleEl = document.createElement("h2");
@@ -50,8 +50,9 @@ blogs.forEach((blog) => {
   // if there is an image url create an img element
   if (blog.image) {
     const img = document.createElement("img");
-    img.src = blog.image;
+    img.src = `blog/${blog.image}`;
     img.alt = blog.imageAlt || blog.title;
+    img.style.width = '10em';
     new_post.appendChild(img);
   }
 
