e82fb8ac684f6f0d3ae7b0e5fc332869dc98a970
Travis Biehle <travissbiehle@gmail.com>
2025-10-16 09:48:02 -0700

    feat: added animation to nav bar

--- bootcamp-milestone-2/src/components/navbar.tsx
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <header className="flex fixed top-0 text-green-800 p-4 w-full z-10 justify-between items-center text-center">
+    <header className="flex sticky top-0 text-green-800 p-4 pb-1 w-full z-10 justify-between items-center text-center">
       <Link href="/" className="hover:text-green-950 text-5xl font-title tracking-tight w-fit">travis biehle</Link>
       <nav className="flex flex-row text-2xl font-sans tracking-normal relative right-3 space-x-5">
-        <Link href="/" className="hover:text-green-950 hover:underline hover:underline-offset-4">home</Link>
-        <Link href="/blog" className="hover:text-green-950 hover:underline hover:underline-offset-4">blog</Link>
-        <Link href="/resume" className="hover:text-green-950 hover:underline hover:underline-offset-4">resume</Link>
-        <Link href="/about" className="hover:text-green-950 hover:underline hover:underline-offset-4">contact</Link>
+        <Link href="/" className="underline underline-offset-4 decoration-transparent hover:decoration-green-950 duration-200 origin-left hover:text-green-950">home</Link>
+        <Link href="/blog" className="underline underline-offset-4 decoration-transparent hover:decoration-green-950 duration-200 origin-left hover:text-green-950">blog</Link>
+        <Link href="/resume" className="underline underline-offset-4 decoration-transparent hover:decoration-green-950 duration-200 origin-left hover:text-green-950">resume</Link>
+        <Link href="/about" className="underline underline-offset-4 decoration-transparent hover:decoration-green-950 duration-200 origin-left hover:text-green-950">contact</Link>
       </nav>
     </header>
   );
