4c4d66003c68e1b40c0d0fafb5b658c484806391
Travis Biehle <travissbiehle@gmail.com>
2025-10-15 19:53:39 -0700

    feat: minor navbar tweak

--- bootcamp-milestone-2/src/components/navbar.tsx
+++ bootcamp-milestone-2/src/components/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <header className="flex sticky text-green-800 p-4 w-full z-10 justify-between items-center text-center">
+    <header className="flex fixed top-0 text-green-800 p-4 w-full z-10 justify-between items-center text-center">
       <Link href="/" className="hover:text-green-950 text-5xl font-title tracking-tight w-fit">travis biehle</Link>
       <nav className="flex flex-row text-2xl font-sans tracking-normal relative right-3 space-x-5">
         <Link href="/" className="hover:text-green-950 hover:underline hover:underline-offset-4">home</Link>
