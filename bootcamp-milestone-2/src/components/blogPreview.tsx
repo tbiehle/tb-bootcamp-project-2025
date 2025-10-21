a9dcec59763ccc071aa591ad84b4de00db41af6e
Travis Biehle <travissbiehle@gmail.com>
2025-10-21 12:06:14 -0700

    feat: blog sizing tweak

--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function BlogPreview(blog: Blog) {
   return (
-    <div className="flex flex-col w-1/3 outline-1 hover:outline-2 outline-green-800 rounded-xl p-2 my-2 mx-4">
+    <div className="flex flex-col w-4/5 md:w-1/2 outline-1 hover:outline-2 outline-green-800 rounded-xl p-2 my-2 mx-4">
       <Link href="" className="flex justify-between m-2 w-auto px-3 py-2">
         <div className="flex flex-col justify-between">
           <div> 
@@ -23,7 +23,7 @@ export default function BlogPreview(blog: Blog) {
           src={blog.image}
           width={100}
           height={100}
-          className="invisible md:visible w-1/3 aspect-square object-cover rounded-2xl"
+          className= "w-1/3 aspect-square object-cover rounded-2xl"
         ></Image>
       </Link>
     </div>
