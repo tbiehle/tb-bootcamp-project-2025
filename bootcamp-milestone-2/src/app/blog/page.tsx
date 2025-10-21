dbb9c2ff223898f372837827b0c085dc7d6cfb21
Travis Biehle <travissbiehle@gmail.com>
2025-10-20 20:51:51 -0700

    padding tweak

--- bootcamp-milestone-2/src/app/blog/page.tsx
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import BlogPreview from "@/components/blogPreview";
 export default function Blog() {
   return (
     <div className="flex flex-col">
-        <h1 className="flex text-9xl font-title text-green-800 justify-center p-5">
+        <h1 className="flex text-9xl font-title text-green-800 justify-center pt-0 p-5">
           blog
         </h1>
         <div className="mb-5">
