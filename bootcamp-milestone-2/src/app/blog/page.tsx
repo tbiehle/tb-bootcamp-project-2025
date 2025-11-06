8bf9fb6d67b32a64713f072bcd7f1420a0585ccb
Travis Biehle <travissbiehle@gmail.com>
2025-11-06 13:06:51 -0800

    feat: getting blog data from MongoDB rather than local storage

--- bootcamp-milestone-2/src/app/blog/page.tsx
+++ bootcamp-milestone-2/src/app/blog/page.tsx
@@ -1,18 +1,34 @@
 import React from "react";
-import blogs from "../../data/blogData";
 import BlogPreview from "@/components/blogPreview";
+import connectDB from "@/database/db";
+import Blog from "@/database/blogSchema";
 
-export default function Blog() {
+async function getBlogs(){
+	await connectDB() // function from db.ts before
+
+	try {
+			// query for all blogs and sort by date
+	    const blogs = await Blog.find().sort({ date: -1 }).orFail()
+			// send a response as the blogs as the message
+	    return blogs
+	} catch (err) {
+	    return null
+	}
+}
+
+export default async function BlogComponent() {
+  const blogs = await getBlogs();
+
   return (
     <div className="flex flex-col">
         <h1 className="flex text-9xl font-title text-green-800 justify-center pt-0 p-5">
           blog
         </h1>
         <div className="mb-5">
-          {blogs.map((blog) => {
+          {blogs && blogs.map((blog) => {
             return (
               <div key={blog.slug} className="flex justify-center">
-                <BlogPreview {...blog} />
+                <BlogPreview blog={blog} />
               </div>
             );
           })}
--- bootcamp-milestone-2/src/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/components/blogPreview.tsx
@@ -1,29 +1,36 @@
 import React from "react";
-import Blog from "@/types/blog";
+import Blog from "@/database/blogSchema";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function BlogPreview(blog: Blog) {
+type Props = {
+  blog: Blog;
+};
+
+export default function BlogPreview({ blog }: Props) {
   return (
     <div className="flex flex-col w-4/5 md:w-1/2 outline-1 hover:outline-2 outline-green-800 rounded-xl p-2 my-2 mx-4">
-      <Link href={`/blog/${blog.slug}`} className="flex justify-between m-2 w-auto px-3 py-2">
+      <Link
+        href={`/blog/${blog.slug}`}
+        className="flex justify-between m-2 w-auto px-3 py-2"
+      >
         <div className="flex flex-col justify-between">
-          <div> 
-          {/* title and description */}
+          <div>
+            {/* title and description */}
             <div className="text-4xl font-title">
               {blog.title.toLowerCase()}
             </div>
             <div>{blog.description}</div>
           </div>
           {/* date */}
-          <div className="text-sm">{blog.date}</div>
+          <div className="text-sm">{blog.date.toString()}</div>
         </div>
         <Image
-          alt={blog.imageAlt}
+          alt={blog.image_alt}
           src={blog.image}
           width={100}
           height={100}
-          className= "w-1/3 aspect-square object-cover rounded-2xl"
+          className="w-1/3 aspect-square object-cover rounded-2xl"
         ></Image>
       </Link>
     </div>
