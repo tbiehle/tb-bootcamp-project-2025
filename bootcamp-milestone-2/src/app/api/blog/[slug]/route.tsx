438631c0f18f70112c5e6fb06c8387e9d197b6f2
Travis Biehle <travissbiehle@gmail.com>
2025-12-11 14:48:58 -0800

    fixed API context

--- bootcamp-milestone-2/src/app/api/blog/[slug]/route.tsx
+++ bootcamp-milestone-2/src/app/api/blog/[slug]/route.tsx
@@ -3,15 +3,15 @@ import connectDB from "@/database/db";
 import { CommentType } from "@/database/commentSchema";
 import Blog from "@/database/blogSchema";
 
-type IParams = {
+interface RouteContext {
   params: {
     slug: string;
   };
-};
+}
 
-export async function GET(req: NextRequest, { params }: IParams) {
+export async function GET(req: NextRequest, context: RouteContext) {
   await connectDB();
-  const { slug } = await params;
+  const { slug } = context.params;
 
   try {
     const blog = await Blog.findOne({ slug }).orFail();
@@ -23,11 +23,11 @@ export async function GET(req: NextRequest, { params }: IParams) {
   }
 }
 
-export async function POST(req: NextRequest, { params }: IParams) {
+export async function POST(req: NextRequest, context: RouteContext) {
   await connectDB();
 
   try {
-    const { slug } = await params;
+    const { slug } = context.params;
     const { user, content } = await req.json();
     const blog = await Blog.findOne({ slug }).orFail();
 
